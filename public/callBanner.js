3e09c05ab1774278da9c2a1df43c7ed5e0790ec0
camilodev09 <camilobau2021@gmail.com>
2024-02-18 19:24:02 -0500

    gocfd

--- public/callBanner.js
+++ public/callBanner.js
@@ -2,6 +2,6 @@
 const phoneNumber = '51966301171'; // Reemplaza con el número de teléfono deseado
 const encodedMessage = encodeURIComponent('I want more information. I´m interested.');
 
-const whatsappButton = document.getElementById('whatsappButton');
+const whatsappButton = document.getElementById('whatsappButtonBanner');
 
 whatsappButton.href = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
